41d9317c40b96f3f942803d1cded6743f1d5e0f4
Tristan Adam <tristan.adam5507@gmail.com>
2025-10-31 11:51:38 +0100

    Initial commit

---
+++ M426_Yael_Dennis_Tristan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M426_Yael_Dennis_Tristan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
